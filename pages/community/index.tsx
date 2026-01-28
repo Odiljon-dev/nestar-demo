f140e2ca17d980f009e25e675416b7fb91d1045f
Odiljon <odiljonzokirov551@gmail.com>
2026-01-28 18:15:48 +0900

    feat: organize and test pages router system

---
+++ pages/community/index.tsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+const Community = () => {
+    console.log("COMMUNITY COMPONENT - PAGES ROUTER")
+ const [title, setTitile] = useState<string>("hello");
+   return <div>COMMUNITY{""}<button onClick={() => alert("Hello MIT")} style={{ margin: "15px" }}>PressMe</button></div>;
+}
+
+export default Community;
