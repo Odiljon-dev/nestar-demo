6a3ff8245833a7b45471646cde46bc9a2b98b907
Odiljon <odiljonzokirov551@gmail.com>
2026-01-30 22:58:17 +0900

    feat: publish main layout components

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,21 +3,19 @@ import { Box, Container, Stack } from "@mui/material";
 import { NextPage } from "next/types";
 
 
-const  Home: NextPage = () => {
+const Home: NextPage = () => {
   return (
+    <Stack className={"home-page"}>
       <Stack>
-        <Stack flexDirection={"column"}>
-          <Stack>
-             <Stack className="container">Popular Properties</Stack>
-          </Stack>
-          <Stack>
-             <Stack className="container">Top Agents</Stack>
-          </Stack>
-          <Stack>
-             <Stack className="container">Top Properties</Stack>
-          </Stack>
-        </Stack>
+        <Stack className="container">Popular Properties</Stack>
       </Stack>
+      <Stack>
+        <Stack className="container">Top Agents</Stack>
+      </Stack>
+      <Stack>
+        <Stack className="container">Top Properties</Stack>
+      </Stack>
+    </Stack>
   );
 };
 
